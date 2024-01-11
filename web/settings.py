f297d2929a2c3ab9e5474f90185cc67136a26109
Daniel Rojas <danielrojas.alvaro2@gmail.com>
2024-01-10 22:46:12 -0400

    test render v.3

--- web/settings.py
+++ web/settings.py
@@ -80,7 +80,6 @@ WSGI_APPLICATION = 'web.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=10000
     )
 }
 
